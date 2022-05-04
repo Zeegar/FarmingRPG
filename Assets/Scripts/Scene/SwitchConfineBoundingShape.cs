ae1b9703ef0f25747ec8204a7fc6457e54653cb2
Perry Hutchison <perryedwinhutchison@gmail.com>
2022-05-04 11:04:01 +0100

    Added a bounding box for the camera, a script to handle switching the bounding box for future scenes. Added scenary objects (tees, bushes and a house)  with collider components added a collision layer to prevent the player walking off the scene

---
+++ Assets/Scripts/Scene/SwitchConfineBoundingShape.cs
@@ -0,0 +1,28 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+public class SwitchConfineBoundingShape : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SwitchBoundingShape();
+    }
+    /// <summary>
+    /// Switch the collider that cinemachine uses to define the edges of the screen
+    /// </summary>
+private void SwitchBoundingShape()
+    {
+        // Get the polygon collider on the boundsconfiner gameobject which is used by cinemachine to prevent the camera going beyond the screen edges
+        PolygonCollider2D polygonCollider2D = GameObject.FindGameObjectWithTag(Tags.BoundsConfiner).GetComponent<PolygonCollider2D>();
+
+        CinemachineConfiner cinemachineConfiner = GetComponent<CinemachineConfiner>();
+
+        cinemachineConfiner.m_BoundingShape2D = polygonCollider2D;
+
+        // since the confiner bounds have changed need to call this to clear the cache
+
+        cinemachineConfiner.InvalidatePathCache();
+    }
+
+}
